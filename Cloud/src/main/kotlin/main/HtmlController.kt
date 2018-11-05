a9dd15a9b283a574ff7d38720e0d617efd80e73d
elumixor <vladogim97@gmail.com>
2018-11-05 17:17:26 +0100

    Kotlin Spring basic controller

---
+++ Cloud/src/main/kotlin/main/HtmlController.kt
@@ -0,0 +1,16 @@
+package main
+
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.ui.set
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
+class HtmlController {
+
+    @GetMapping("/")
+    fun blog(model: Model): String {
+        model["title"] = "Hello, world!"
+        return "content"
+    }
+}
